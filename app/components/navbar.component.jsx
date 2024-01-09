4cde85aff845d820eb25474e20e43df0676e05a8
kanishkapasi <kanishkapasindu6@gmail.com>
2024-01-09 16:00:29 +0530

    'kpdev'

--- app/components/navbar.component.jsx
+++ app/components/navbar.component.jsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 import imageOne from "../../public/my_logo/20240109_103944-removebg-preview.png";
 
 export default function Navbar() {
   return (
-    <div>
-      <Image src={imageOne} />
-      <h1>Navbar</h1>
+    <div className="w-[100% h-[10vh] bg-black hidden lg:block ">
+      <div className="w-[80%] h-[10vh]  mx-auto text-light bg-black flex items-center justify-center">
+        <Link href={"/"}>
+          <div className="w-[170px] h-[50px]">
+            <Image src={imageOne} className="w-96 pb-1" />
+          </div>
+        </Link>
+      </div>
     </div>
   );
 }
