fe827c585f0fed4cb7b4970c8a950246961ae430
kanishkapasi <kanishkapasindu6@gmail.com>
2024-01-09 18:56:58 +0530

    'kpdev'

--- app/page.jsx
+++ app/page.jsx
@@ -9,6 +9,24 @@ import SideBar from "./components/sidebar.component";
 import Image from "next/image";
 import codeImage from "../public/my_logo/icon-dev.svg";
 
+const data = [
+  {
+    id: "1",
+    h3: "Website Development",
+    p: "Ecommerce websites,Personal websites,Portfolio websites,Small business websites,Blog websites.",
+  },
+  {
+    id: "2",
+    h3: "Website Development",
+    p: "Ecommerce websites,Personal websites,Portfolio websites,Small business websites,Blog websites.",
+  },
+  {
+    id: "3",
+    h3: "Website Development",
+    p: "Ecommerce websites,Personal websites,Portfolio websites,Small business websites,Blog websites.",
+  },
+];
+
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
 
@@ -27,50 +45,26 @@ export default function Home() {
           <Navbar />
 
           <Header />
-          <div className="flex items-center justify-between">
+          <div className="flex items-start justify-between">
             <SideBar />
-            <div className="mt-4 bg-[#0e0d0d] lg:w-[60%] w-[80%] mx-auto h-[80vh] rounded-[30px]">
+            <div className="mt-4 lg:mt-0 bg-[#0e0d0d] lg:w-[60%] w-[80%] mx-auto h-[200vh] rounded-[30px]">
               <h2 className="text-center text-white text-2xl mt-3">Services</h2>
-              <div className="w-[90%] mx-auto h-[100%] grid lg:grid-cols-3 gap-3 md:grid-cols-2 grid-cols-1 mt-4">
-                <div className="w-[100%] h-[20vh] lg:h-[30vh]  rounded-[20px] bg-black p-2 flex items-start justify-around gap-1 pt-5">
-                  <Image src={codeImage} />
-                  <div className="w-[80%] flex-col items-center justify-center">
-                    <h3 className="text-white text-md md:text-xl">
-                      Website Development
-                    </h3>
-                    <div className="w-[100%] border-b-4 border-[#0e0d0d]"></div>
-                    <p className="text-white">
-                      Ecommerce websites,Personal websites,Portfolio
-                      websites,Small business websites,Blog websites.
-                    </p>
+              <div className="w-[90%] mx-auto grid lg:grid-cols-3 gap-3 md:grid-cols-2 grid-cols-1 mt-4">
+                {data.map((e) => (
+                  <div
+                    className="w-[100%] h-[20vh] lg:h-[30vh]  rounded-[20px] bg-black p-2 flex items-start justify-around gap-1 pt-5"
+                    key={e.id}>
+                    <Image src={codeImage} />
+                    <div className="w-[80%] flex-col items-center justify-center">
+                      <h3 className="text-white text-md md:text-xl">{e.h3}</h3>
+                      <div className="w-[100%] border-b-4 border-[#0e0d0d]"></div>
+                      <p className="text-white">{e.p}</p>
+                    </div>
                   </div>
-                </div>
-                <div className="w-[100%] h-[20vh] lg:h-[30vh] rounded-[20px] bg-black p-2 flex items-start justify-around gap-1 pt-5">
-                  <Image src={codeImage} />
-                  <div className="w-[80%] flex-col items-center justify-center">
-                    <h3 className="text-white text-2xl">Website Development</h3>
-                    <div className="w-[100%] border-b-4 border-[#0e0d0d]"></div>
-                    <p className="text-white">
-                      Ecommerce websites,Personal websites,Portfolio
-                      websites,Small business websites,Blog websites.
-                    </p>
-                  </div>
-                </div>
-                <div className="w-[100%] h-[20vh] lg:h-[30vh] rounded-[20px] bg-black p-2 flex items-start justify-around gap-1 pt-5">
-                  <Image src={codeImage} />
-                  <div className="w-[80%] flex-col items-center justify-center">
-                    <h3 className="text-white text-2xl">Website Development</h3>
-                    <div className="w-[100%] border-b-4 border-[#0e0d0d]"></div>
-                    <p className="text-white">
-                      Ecommerce websites,Personal websites,Portfolio
-                      websites,Small business websites,Blog websites.
-                    </p>
-                  </div>
-                </div>
+                ))}
               </div>
             </div>
           </div>
-
           {/* Second Page */}
         </div>
       )}
